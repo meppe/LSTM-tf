dbc09efd39713d02934585a04f694759a8e2bdd3
Manfred Eppe <manfred@eppe.eu>
2016-09-05 15:52:06 -0700

    modification on Mon 09/05/2016 15:52:05.59

--- ptb/ptb_word_lm.py
+++ ptb/ptb_word_lm.py
@@ -406,9 +406,7 @@ def main(_):
             test_perplexity = run_epoch(session, mtest, test_data, tf.no_op(), r)
             print("Test Perplexity: %.8f" % test_perplexity)
 
-            model_path = "/home/meppe/Coding/LSTM-tf/ptb/models"+str(time())
-            if not gfile.IsDirectory(os.path.dirname(model_path)):
-                system("mkdir " +model_path)
+            model_path = "/home/meppe/Coding/LSTM-tf/ptb/model_"+str(time.clock())
             save_path = saver.save(session, model_path)
             print("Model saved in path: %s" % save_path)
             # system("ln -s "+model_path+" last_model")
